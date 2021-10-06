6bafcca80979420b471245fa3e4065fa456e6eea
Alex Liu <alex.liu245@gmail.com>
2021-10-05 22:21:35 -0400

    More setup
    - setup eslint and some default rules
    - added some default boilerplate sass files
    - removed random create-react-app boilerplate

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./header.scss";
+
+const Header = () => {
+	return (
+		<div className="header-body">
+			<p>TESTING</p> 
+		</div>
+	);
+}
+
+export default Header;
---
+++ src/consts/item_info.ts
@@ -0,0 +1,4 @@
+// const file to store item stats in
+export const info: Record<string, number> = {
+	"test" : 5,
+}
